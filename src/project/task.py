614d44f33984980278e887caf750757004128101
Neal Morton <mortonne@gmail.com>
2025-02-19 20:07:46 -0600

    feat: functions to work with task data

--- src/project/task.py
+++ src/project/task.py
@@ -1,5 +1,49 @@
-from pkg_resources import resource_filename
+from importlib import resources
+import polars as pl
 
 
 def get_data_file():
-    data_file = resource_filename("project", "data/exp1.csv")
+    """Get path to the raw data file."""
+    data_file = resources.files("project").joinpath("data/exp1.csv")
+    return data_file
+
+
+def read_data(data_file):
+    """Read data and fix formatting."""
+    raw = pl.read_csv(data_file)
+    columns = [
+        "subj",
+        "cycle",
+        "trial",
+        "phase",
+        "type",
+        "word1",
+        "word2",
+        "response",
+        "RT",
+        "correct",
+        "lag",
+    ]
+    df = raw.select(pl.col(columns)).with_columns(
+        pl.col("response", "RT", "correct", "lag").replace(-1, None)
+    )
+    return df
+
+
+def exclude_fast_responses(df, thresh=0.2):
+    """Exclude responses that were too quick to be reactions."""
+    df_filt = (
+        df.with_columns(
+            pl.when(pl.col("RT") < thresh)
+            .then(None)
+            .otherwise(pl.col("RT"))
+            .alias("RT"),
+        )
+        .with_columns(
+            pl.when(pl.col("RT").is_null())
+            .then(None)
+            .otherwise(pl.col("response"))
+            .alias("response")
+        )
+    )
+    return df_filt
